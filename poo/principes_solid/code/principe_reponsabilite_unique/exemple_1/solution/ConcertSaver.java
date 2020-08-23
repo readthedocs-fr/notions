7e8a614813cf2cf07f49300bd5dec508203679e5
LukaMrt <maret.luka@gmail.com>
2020-08-23 19:37:18 +0200

    fix(S principle): update examples to pseudo code

--- poo/principes_solid/code/principe_reponsabilite_unique/exemple_1/solution/ConcertSaver.java
+++
@@ -1,7 +0,0 @@
-public class ConcertSaver {
-
-    public void saveConcert(ConcertHall concertHall, String day) {
-        // Sauvegarde dans un fichier des données du spectacle en rapport avec la salle
-    }
-
-}
--- poo/principes_solid/code/principe_reponsabilite_unique/exemple_1/solution/RowsCalculator.java
+++
@@ -1,9 +0,0 @@
-public class RowsCalculator {
-
-    private static final int SEAT_PER_ROW = 10;
-
-    public int calculRows(ConcertHall concertHall) {
-        return (concertHall.getClassicSeats() + concertHall.getHandicapSeats() + concertHall.getClassicSeats()) / SEAT_PER_ROW;
-    }
-
-}
